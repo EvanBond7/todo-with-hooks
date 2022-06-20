9d4848a93394e29478e63b9a6914caf3482bfbfe
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-06-20 12:06:10 +0300

    Icons without functionality

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -16,6 +16,17 @@ const TodoList = ({ todos, setTodos }) => {
             className='list'
             onChange={handleTask}
           />
+          <div>
+            <button className='button-complete task-button'>
+              <i className='fa fa-check-circle'></i>
+            </button>
+            <button className='button-edit task-button'>
+              <i className='fa fa-edit'></i>
+            </button>
+            <button className='button-delete task-button'>
+              <i className='fa fa-trash'></i>
+            </button>
+          </div>
         </li>
       ))}
     </div>
