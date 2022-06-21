2b5150694ff38f691e3fc60493d77dac111a4773
Evan Bond <43074752+EvanBond7@users.noreply.github.com>
2022-06-21 16:02:21 +0300

    Editing tasks

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
-const Form = ({ task, setTask, todos, setTodos }) => {
+const Form = ({ task, setTask, todos, setTodos, editTodo, setEditTodo }) => {
   const handleChangeTask = (e) => {
     setTask(e.target.value);
   };
 
+  useEffect(() => {
+    if (editTodo) {
+      setTask(editTodo.title);
+    } else {
+      setTask('');
+    }
+  }, [setTask, editTodo]);
+
+  const updateTodo = (title, id, completed) => {
+    const newTodo = todos.map((todo) => {
+      todo.id === id ? { title, id, completed } : todo;
+    });
+    setTodos(newTodo);
+    setEditTodo('');
+  };
+
   const onFormSubmit = (e) => {
     e.preventDefault();
-    setTodos([...todos, { id: nanoid(), title: task, completed: false }]);
-    setTask('');
+    if (!editTodo) {
+      setTodos([...todos, { id: nanoid(), title: task, completed: false }]);
+      setTask('');
+    } else {
+      updateTodo(task, editTodo.id, editTodo.completed);
+    }
   };
 
   return (
@@ -23,7 +43,7 @@ const Form = ({ task, setTask, todos, setTodos }) => {
         onChange={handleChangeTask}
       />
       <button className='button-add' type='submit'>
-        +
+        {editTodo ? '💻' : '+'}
       </button>
     </form>
   );
